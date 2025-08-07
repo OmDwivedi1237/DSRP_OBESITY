968ad67c021192bae71d85567aef2c24da0f933a
OmDwivedi1237 <26dwivediom@gmail.com>
2025-08-07 01:45:49 -0700

    cleaned data (made new csv)

---
+++ src/clean_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('data/obesity.csv')
+
+df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+
+categorical_cols = ['gender', 'family_history_with_overweight', 'favc', 'caec', 'smoke', 'scc', 'calc', 'mtrans', 'nobeyesdad']
+
+for col in categorical_cols:
+    df[col] = df[col].astype('category')
+
+df.to_csv("data/obesity_cleaned.csv", index=False)
+print("###################### CLEANED DAT ######################")
